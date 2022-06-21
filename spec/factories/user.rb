4c396f29386a26cc59362a6675d8e7d4acb6d33d
yusukedayo <tsunemi0217@icloud.com>
2022-06-21 19:24:05 +0900

    Add: モデルspecを追加

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+		sequence(:name) { |n| "name_#{n}" }
+    sequence(:email) { |n| "test#{n}@example.com" }
+		profile{ 'こんにちは！初めまして！'}
+    password{ 'password' }
+  end
+end
