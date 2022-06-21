c2b55687c8d5979d0cc7b1bbb345bfbcf4bb18cd
yusukedayo <tsunemi0217@icloud.com>
2022-06-21 19:28:28 +0900

    Add: ログインマクロを追加

---
+++ spec/support/login_macros.rb
@@ -0,0 +1,9 @@
+module LoginMacros
+  def login_as(user)
+    visit login_path
+    click_link 'ログイン'
+    fill_in 'email', with: user.email
+    fill_in 'password', with: 'password'
+    click_button 'ログイン'
+  end
+end
