20b5b90618d3798aa2aa0e56863386aa2c295bd3
yusukedayo <tsunemi0217@icloud.com>
2022-06-22 18:54:55 +0900

    Add: deviceの設定を変更

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "name_#{n}" }
     sequence(:email) { |n| "test#{n}@example.com" }
-    profile { 'こんにちは！初めまして！' }
     password { 'password' }
   end
 end
