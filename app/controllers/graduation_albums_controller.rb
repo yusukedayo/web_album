c468e34f5277c68266796b4dcb9920230b25a95c
yusukedayo <tsunemi0217@icloud.com>
2022-06-23 19:31:57 +0900

    Add: graduation_albumsコントローラーの追加

---
+++ app/controllers/graduation_albums_controller.rb
@@ -0,0 +1,14 @@
+class GraduationAlbumsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @graduation_album = GraduationAlbum.new
+  end
+
+  def edit
+  end
+end
