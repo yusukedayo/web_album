27b72931ddbf5e6c959f8760de1874cb1e702c67
yusukedayo <tsunemi0217@icloud.com>
2022-06-23 20:10:44 +0900

    Add: アルバム新規作成・一覧表示機能の追加

--- app/controllers/graduation_albums_controller.rb
+++ app/controllers/graduation_albums_controller.rb
@@ -1,5 +1,6 @@
 class GraduationAlbumsController < ApplicationController
   def index
+    @graduation_albums = GraduationAlbum.all.order(created_at: :desc)
   end
 
   def show
@@ -9,6 +10,24 @@ class GraduationAlbumsController < ApplicationController
     @graduation_album = GraduationAlbum.new
   end
 
+  def create
+    @graduation_album = current_user.graduation_albums.build(graduation_album_params)
+    if @graduation_album.save
+      redirect_to graduation_albums_path, notice: '作成に成功しました'
+    else
+      flash.now['alert'] = '作成に失敗しました'
+      render :new
+    end
+  end
+
+
+
   def edit
+  end
+
+  private
+
+  def graduation_album_params 
+    params.require(:graduation_album).permit(:album_name, :title)
   end
 end
