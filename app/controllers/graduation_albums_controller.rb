07739f4de4fe2b90a6e72bca8d011571f3f623da
yusukedayo <tsunemi0217@icloud.com>
2022-06-24 22:16:24 +0900

    refactor: N+1問題を解決するためにincludeメソッドを追加

--- app/controllers/graduation_albums_controller.rb
+++ app/controllers/graduation_albums_controller.rb
@@ -2,7 +2,7 @@ class GraduationAlbumsController < ApplicationController
   before_action :set_graduation_album, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: %i[new create edit update destroy]
   def index
-    @graduation_albums = GraduationAlbum.all.order(created_at: :desc)
+    @graduation_albums = GraduationAlbum.all.includes(:user).order(created_at: :desc)
   end
 
   def show; end
