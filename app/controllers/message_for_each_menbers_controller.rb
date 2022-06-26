09e36c293c5358476fbbc769d41ab40dae835278
yusukedayo <tsunemi0217@icloud.com>
2022-06-26 14:40:46 +0900

    feat: 個別メッセージ機能のため、コントローラーを追加 #31

---
+++ app/controllers/message_for_each_menbers_controller.rb
@@ -0,0 +1,36 @@
+class MessageForEachMenbersController < ApplicationController
+  before_action :authenticate_user!, only: %i[create edit update destroy]
+  before_action :set_message_for_each_menber, only: %i[edit update destroy]
+
+  def create
+    @message_for_each_menber = current_user.message_for_each_menbers.build(message_for_each_menber_params)
+    if @message_for_each_menber.save
+      redirect_to graduation_album_path(@message_for_each_menber.graduation_album), success: 'コメントを投稿しました'
+    else
+      redirect_to graduation_album_path(@message_for_each_menber.graduation_album), danger: 'コメントの投稿に失敗しました'
+    end
+  end
+
+  def edit; end
+
+  def update
+    @message_for_each_menber.update(message_for_each_menber_update_params)
+  end
+
+  def destroy
+    @message_for_each_menber.destroy!
+  end
+
+  private
+  def set_message_for_each_menber
+    @message_for_each_menber = current_user.message_for_each_menbers.find(params[:id])
+  end
+
+  def message_for_each_menber_params
+    params.require(:message_for_each_menber).permit(:body).merge(graduation_album_id: params[:graduation_album_id])
+  end
+
+  def message_for_each_menber_update_params
+    params.require(:message_for_each_menber).permit(:body)
+  end
+end
