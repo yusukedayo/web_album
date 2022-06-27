42a53390d13f333be04086d632453d10f8d55dad
yusukedayo <tsunemi0217@icloud.com>
2022-06-27 17:47:05 +0900

    test:個別メッセージ機能のモデル・システムテストを追加#31

---
+++ spec/factories/message_for_each_menber.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :message_for_each_menber do
+    sequence(:body) { |n| "message_#{n}" }
+    to_user { user.id }
+    user
+    graduation_album
+  end
+end
--- spec/system/message_for_each_menbers_spec.rb
+++ spec/system/message_for_each_menbers_spec.rb
@@ -1,28 +1,25 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe 'MessageForEveryones', type: :system do
+RSpec.describe "MessageForEachMenbers", type: :system do
   let(:user) { create(:user) }
   let(:graduation_album) { create(:graduation_album, user:) }
   let(:message_for_everyone) { create(:message_for_everyone, user:, graduation_album:) }
-
   describe 'ログイン後' do
     before do
       login_as(user)
     end
-    describe 'メッセージの新規作成' do
-      context 'メッセージの入力値が正常' do
-        it 'メッセージの新規作成が成功する' do
-          visit graduation_album_path(graduation_album)
+    describe '個別メッセージの新規作成' do
+      context '個別メッセージの入力値が正常' do
+        it '個別メッセージの新規作成が成功する' do
+          visit graduation_album_menber_path(graduation_album, user)
           fill_in 'メッセージ', with: 'メッセージテスト'
           click_button '作成する'
-          visit graduation_album_path(graduation_album)
+          visit graduation_album_menber_path(graduation_album, user)
           expect(page).to have_content 'メッセージテスト'
         end
       end
       context 'bodyが未入力' do
-        it 'メッセージの新規作成が失敗する' do
+        it '個別メッセージの新規作成が失敗する' do
           visit graduation_album_path(graduation_album)
           fill_in 'メッセージ', with: nil
           click_button '作成する'
