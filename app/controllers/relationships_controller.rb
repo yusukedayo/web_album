5e40883ed0d45d19644ce8241e58a5dc505596cc
yusukedayo <tsunemi0217@icloud.com>
2022-06-29 18:52:32 +0900

    Change: フォロー後のredirect先を変更#43

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,31 +1,26 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user
 
   def create
+    @user = User.find(params[:follow_id])
     following = current_user.follow(@user)
     if following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      redirect_to graduation_albums_path
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+      redirect_to graduation_albums_path
     end
   end
 
   def destroy
+    @user = User.find(params[:follow_id])
     following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      redirect_to graduation_albums_path
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+      redirect_to graduation_albums_path
     end
   end
-
-  private
-  def
-    @user = User.find(params[:relationship][:follow_id])
-  end
-
 end
