9a6253499a0e98728597844a0e9e3dbff07ffa92
yusukedayo <tsunemi0217@icloud.com>
2022-07-03 22:45:31 +0900

    Fix: s3のブラケット名を修正#49

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'S3のバケット名'
+  config.fog_directory  = 'aws-test-rails'
 	config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
