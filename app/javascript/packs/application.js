9be1cd0ce9d88a4b79c46179f6e41de5a43711ff
yusukedayo <tsunemi0217@icloud.com>
2022-07-06 16:36:01 +0900

    feat: fontawsome実装のため設定を編集#52

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import '@fortawesome/fontawesome-free/js/all';
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
