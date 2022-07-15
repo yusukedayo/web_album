b2c3109d45a2fb6a9282ed4833c1a751d11857c2
yusukedayo <tsunemi0217@icloud.com>
2022-07-15 14:46:00 +0900

    Fix: ユーザープロフィール編集で変更が行われないバグを解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar face_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar face_id])
   end
 end
