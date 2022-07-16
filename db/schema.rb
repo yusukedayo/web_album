52ca4e99069826145daeb4797c3c5d05d77d6da3
yusukedayo <tsunemi0217@icloud.com>
2022-07-16 20:01:38 +0900

    Delete: 画像機能をactive_storageに変更したのでphotosカラムを削除#66

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_131803) do
+ActiveRecord::Schema.define(version: 2022_07_16_105541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_131803) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.json "photos"
     t.index ["user_id"], name: "index_graduation_albums_on_user_id"
   end
 
