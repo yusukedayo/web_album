125f59df6bff694a463b87047dfcf4b18b1b43e2
yusukedayo <tsunemi0217@icloud.com>
2022-07-18 08:30:03 +0900

    Fix: ユーザーをフォロー後のリダイレクト先をユーザー詳細画面に修正#74

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,22 +2,14 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:follow_id])
     following = current_user.follow(@user)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-    end
-    redirect_to graduation_albums_path
+    following.save!
+    redirect_to users_show_path
   end
 
   def destroy
     @user = User.find(params[:follow_id])
     following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-    end
-    redirect_to graduation_albums_path
+    following.destroy!
+    redirect_to users_show_path
   end
 end
