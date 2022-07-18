280b85cefcd9f254f6f48eaad260edbe5c958d5b
yusukedayo <tsunemi0217@icloud.com>
2022-07-18 12:18:32 +0900

    feat: ユーザー検索機能実装のため、ransackを追加#74

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
     @user = current_user
     @following_users = @user.followings
     @followed_users = @user.followers
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
 end
