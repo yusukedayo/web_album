7303138f0cede8c8dc636e16420ea31d8625fb35
yusukedayo <tsunemi0217@icloud.com>
2022-07-30 22:45:40 +0900

    Fix: rspecに合わせてリンク名を修正

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -3,7 +3,6 @@
 module LoginMacros
   def login_as(user)
     visit login_path
-    click_link 'ログイン'
     fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
     click_button 'ログイン'
