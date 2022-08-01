9ce07139d28aac721fe0794ed7ccdc33eb6b9fbb
yusukedayo <tsunemi0217@icloud.com>
2022-08-02 00:58:42 +0900

    refactor: rubocopを実行

--- app/controllers/auto_make_ranks_controller.rb
+++ app/controllers/auto_make_ranks_controller.rb
@@ -4,7 +4,7 @@ class AutoMakeRanksController < ApplicationController
       flash[:danger] = 'イベント作成するためには写真を追加してください'
       redirect_back(fallback_location: root_path)
     else
-			#既にhappy_scoreが設定されているものは除外する
+      # 既にhappy_scoreが設定されているものは除外する
       having_face_images = PhotoPath.where(graduation_album_id: params[:graduation_album_id]).where(happy_score: nil)
       if having_face_images
         credentials = Aws::Credentials.new(
@@ -24,10 +24,10 @@ class AutoMakeRanksController < ApplicationController
           }
           result = client.detect_faces attrs
           number = if result.to_h[:face_details].length > 5
-                    5
-                  else
-                    result.to_h[:face_details].length
-                  end
+                     5
+                   else
+                     result.to_h[:face_details].length
+                   end
           happy_score_count = 0
           number.times do |count|
             happy_score_count += result.to_h[:face_details][count][:emotions][0][:confidence].round
@@ -43,8 +43,8 @@ class AutoMakeRanksController < ApplicationController
       else
         @rank = Rank.create!(rank_title: 'ハッピー写真ランキング', rank_description: 'アルバムに投稿された写真から笑顔の写真を抜粋しました！みんなの笑顔の写真を見て癒されましょう！', user_id: current_user.id, graduation_album_id: params[:graduation_album_id])
         happy_photos.each do |photo|
-					rank_choice = RankChoice.create!(content: "#{photo.happy_score}点の笑顔です！", rank_id: @rank.id)
-					ActiveStorage::Attachment.create!(name: 'rank_choice_image', record_type: 'RankChoice', record_id: rank_choice.id, blob_id: photo.path.to_i)
+          rank_choice = RankChoice.create!(content: "#{photo.happy_score}点の笑顔です！", rank_id: @rank.id)
+          ActiveStorage::Attachment.create!(name: 'rank_choice_image', record_type: 'RankChoice', record_id: rank_choice.id, blob_id: photo.path.to_i)
         end
         redirect_to graduation_album_rank_path(@rank.graduation_album, @rank), notice: 'イベントを作成しました'
       end
